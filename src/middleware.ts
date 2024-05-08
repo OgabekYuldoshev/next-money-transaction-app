30737fe594f58042ac746acbd2501945f80d551f
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-05-08 17:44:21 +0500

    build: ✨ change urls

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export const config = {
-  matcher: ["/dashboard/:path*", "/callback"],
-};
-
 export default authMiddleware;
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/auth-callback"],
+};
